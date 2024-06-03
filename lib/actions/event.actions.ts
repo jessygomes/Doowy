d846708185bab7880bdc61dc9fbedbdf2fda122b
Jessy <you@example.com>
2024-06-03 14:39:49 +0200

    EventDetailPage

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -5,6 +5,17 @@ import { handleError } from "../utils";
 import { connectToDb } from "../mongoDb/database";
 import User from "../mongoDb/database/models/User";
 import Event from "../mongoDb/database/models/Event";
+import Category from "../mongoDb/database/models/Category";
+
+const populateEvent = async (query: any) => {
+  return query
+    .populate({
+      path: "organizer",
+      model: User,
+      select: "_id firstName lastName",
+    })
+    .populate({ path: "category", model: Category, select: "_id name" });
+};
 
 export const createEvent = async ({
   event,
@@ -27,6 +38,22 @@ export const createEvent = async ({
     });
 
     return JSON.parse(JSON.stringify(newEvent));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getEventById = async (eventId: string) => {
+  try {
+    await connectToDb();
+
+    const event = await populateEvent(Event.findById(eventId));
+
+    if (!event) {
+      throw new Error("Event not found");
+    }
+
+    return JSON.parse(JSON.stringify(event));
   } catch (error) {
     handleError(error);
   }
--- lib/utils.ts
+++ lib/utils.ts
@@ -16,7 +16,7 @@ export const formatDateTime = (dateString: Date) => {
     day: "numeric", // numeric day of the month (e.g., '25')
     hour: "numeric", // numeric hour (e.g., '8')
     minute: "numeric", // numeric minute (e.g., '30')
-    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+    hour12: false, // use 12-hour clock (true) or 24-hour clock (false)
   };
 
   const dateOptions: Intl.DateTimeFormatOptions = {
@@ -29,21 +29,21 @@ export const formatDateTime = (dateString: Date) => {
   const timeOptions: Intl.DateTimeFormatOptions = {
     hour: "numeric", // numeric hour (e.g., '8')
     minute: "numeric", // numeric minute (e.g., '30')
-    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+    hour12: false, // use 12-hour clock (true) or 24-hour clock (false)
   };
 
   const formattedDateTime: string = new Date(dateString).toLocaleString(
-    "en-US",
+    "fr-FR",
     dateTimeOptions
   );
 
   const formattedDate: string = new Date(dateString).toLocaleString(
-    "en-US",
+    "fr-FR",
     dateOptions
   );
 
   const formattedTime: string = new Date(dateString).toLocaleString(
-    "en-US",
+    "fr-FR",
     timeOptions
   );
 

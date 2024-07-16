c342a29c9605e1bf8da49b6b6d08a292dbde0702
Jessy <you@example.com>
2024-07-16 16:57:55 +0200

    fix

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,5 +1,11 @@
-import { SearchParamProps } from "@/types";
+// import { SearchParamProps } from "@/types";
 import { EventPresentation } from "@/components/shared/EventPresentation";
+
+export type SearchParamProps = {
+  page?(page: any): unknown;
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
 export default function EventDetail({
   params: { id },
